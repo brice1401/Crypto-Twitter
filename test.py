11ee2170404340a3bfc6a602323920db62bbadfe
Louis Melchior <lpglm@orange.fr>
2021-04-25 17:00:31 +0200

    Change the credentials handling, add logs, refactor into functions

--- test.py
+++ test.py
@@ -1,50 +1,80 @@
-# Code venant de :
-# https://towardsdatascience.com/extracting-data-from-twitter-using-python-5ab67bff553a
-
-# import files and give access to tokens and keys
+"""
+Code inspired by: https://towardsdatascience.com/extracting-data-from-twitter-using-python-5ab67bff553a
+"""
 
 import json
+from typing import IO, Optional, List, Any
+import logging
+import sys
 
 import tweepy
-from config import *
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+if sys.version_info < (3, 8):
+    raise RuntimeError("You have to run this code with at least python3.8")
 
-auth.set_access_token(access_token, access_token_secret)
+logging.basicConfig(level="INFO")
+log = logging.getLogger(__name__)
 
-tweet_list = []
+with open("credentials.json", "r") as credentials_file:
+    credentials = json.load(credentials_file)
+
+
+def get_twitter_authentication() -> Any:
+    log.info("Configuring the twitter authentication")
+    twitter_authentication = tweepy.OAuthHandler(credentials["consumer_key"], credentials["consumer_secret"])
+    twitter_authentication.set_access_token(credentials["access_token"], credentials["access_token_secret"])
+    return twitter_authentication
 
 
 class MyStreamListener(tweepy.StreamListener):
-    def __init__(self, api=None):
-        super(MyStreamListener, self).__init__()
-        self.num_tweets = 0
-        self.file = open("tweet.txt", "w")
 
-    def on_status(self, status):
+    def __init__(self, api=None, max_num_status: int = 100, filename_cache: str = "tweet.txt"):
+        super(MyStreamListener, self).__init__()
+        self.filename_cache = filename_cache
+        self.file_cache: Optional[IO] = None
+        self.num_tweets = 0
+        self.max_num_status = max_num_status
+
+    def on_status(self, status) -> bool:
         tweet = status._json
-        self.file.write(json.dumps(tweet) + '\n')
-        tweet_list.append(status)
+        self.store_in_cache(tweet=tweet)
         self.num_tweets += 1
-        if self.num_tweets < 10:
+        if self.num_tweets < self.max_num_status:
+            # While the number of max tweet is not reached, it is not stopped
             return True
         else:
+            log.info(f"The maximum number of tweet (statues) {self.max_num_status=} to read has been reached, the StreamListener stops to save the tweets")
+            self.file_cache.close()
             return False
-        self.file.close()
+
+    def store_in_cache(self, tweet: dict) -> None:
+        if not self.file_cache:
+            self.file_cache = open(self.filename_cache, "w")
+            self.num_tweets = 0
+        self.file_cache.write(json.dumps(tweet) + '\n')
 
 
-# create streaming object and authenticate
-l = MyStreamListener()
-stream = tweepy.Stream(auth, l)
-# this line filters twiiter streams to capture data by keywords
-stream.filter(track=['covid', 'corona', 'covid19', 'coronavirus', 'facemask', 'sanitizer', 'social-distancing'])
+def listen_for_keywords(keywords: List[str], max_num_status: int = 100) -> None:
+    twitter_authentication = get_twitter_authentication()
+    log.info("Create streaming object and authenticate")
+    twitter_listener = MyStreamListener(max_num_status=max_num_status)
+    stream = tweepy.Stream(twitter_authentication, twitter_listener)
+    log.info(f"Launch the stream processor with the tracker set to follow the {keywords=}")
+    stream.filter(track=keywords)
+    log.info(f"The stream stopped")
 
-tweets_data_path = 'tweet.txt'
-tweets_data = []
-tweets_file = open(tweets_data_path, "r")
-# read in tweets and store on list
-for line in tweets_file:
-    tweet = json.loads(line)
-    tweets_data.append(tweet)
-tweets_file.close()
-print(tweets_data[0])
+
+# tweets_data_path = 'tweet.txt'
+# tweets_data = []
+# tweets_file = open(tweets_data_path, "r")
+# # read in tweets and store on list
+# for line in tweets_file:
+#     tweet = json.loads(line)
+#     tweets_data.append(tweet)
+# tweets_file.close()
+# print(tweets_data[0])
+
+if __name__ == "__main__":
+    keywords_to_watch = ['covid', 'corona', 'covid19', 'coronavirus', 'facemask', 'sanitizer', 'social-distancing']
+    listen_for_keywords(keywords=keywords_to_watch, max_num_status=100)
+    print("done")
