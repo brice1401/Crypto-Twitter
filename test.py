77285473793e087adbd9fc741b27dcea5b354219
Louis Melchior <lpglm@orange.fr>
2021-04-25 15:13:57 +0200

    Reformat file, remove dirty file, change gitignore

--- test.py
+++ test.py
@@ -1,45 +1,50 @@
 # Code venant de :
 # https://towardsdatascience.com/extracting-data-from-twitter-using-python-5ab67bff553a
 
-#import files and give access to tokens and keys
+# import files and give access to tokens and keys
 
-import tweepy,json
+import json
+
+import tweepy
 from config import *
 
-auth= tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-auth.set_access_token(access_token,access_token_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-tweet_list=[]
+tweet_list = []
+
+
 class MyStreamListener(tweepy.StreamListener):
-    def __init__(self,api=None):
-        super(MyStreamListener,self).__init__()
-        self.num_tweets=0
-        self.file=open("tweet.txt","w")
-    def on_status(self,status):
-        tweet=status._json
-        self.file.write(json.dumps(tweet)+ '\n')
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
+        self.file = open("tweet.txt", "w")
+
+    def on_status(self, status):
+        tweet = status._json
+        self.file.write(json.dumps(tweet) + '\n')
         tweet_list.append(status)
-        self.num_tweets+=1
-        if self.num_tweets<10:
+        self.num_tweets += 1
+        if self.num_tweets < 10:
             return True
         else:
             return False
         self.file.close()
 
 
-#create streaming object and authenticate
+# create streaming object and authenticate
 l = MyStreamListener()
-stream =tweepy.Stream(auth,l)
-#this line filters twiiter streams to capture data by keywords
-stream.filter(track=['covid','corona','covid19','coronavirus','facemask','sanitizer','social-distancing'])
+stream = tweepy.Stream(auth, l)
+# this line filters twiiter streams to capture data by keywords
+stream.filter(track=['covid', 'corona', 'covid19', 'coronavirus', 'facemask', 'sanitizer', 'social-distancing'])
 
-tweets_data_path='tweet.txt'
-tweets_data=[]
-tweets_file=open(tweets_data_path,"r")
-#read in tweets and store on list
+tweets_data_path = 'tweet.txt'
+tweets_data = []
+tweets_file = open(tweets_data_path, "r")
+# read in tweets and store on list
 for line in tweets_file:
-    tweet=json.loads(line)
+    tweet = json.loads(line)
     tweets_data.append(tweet)
 tweets_file.close()
 print(tweets_data[0])
